2cbfe5cff31875f2bdd32cda014128c55ec07701
Ation <gation@gmail.com>
2015-05-22 11:38:03 +0300

    Fix download for all possible pages

--- download.js
+++ download.js
@@ -1,7 +1,21 @@
-
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     if (msg.text && (msg.text == "download_media")) {
-      var downloadLink = downloadLink = document.querySelectorAll('head meta[property="og:'+ 'image' +'"]')[0].content;
+      var type = document.querySelectorAll('head meta[property="og:type"]')[0].content;
+      var downloadLink = ''
+      switch(type) {
+      case "video":
+        downloadLink = document.querySelectorAll('head meta[property="og:'+ 'video' +'"]')[0].content;
+        break;
+      case "profile":
+      case "instapp:photo":
+        downloadLink = document.body.querySelectorAll(".lfFrame")[0].getAttribute("src");
+        if (downloadLink === null) {
+          downloadLink = document.body.querySelectorAll(".utiImage")[0].getAttribute("src");
+        }
+        break;
+      default:
+        //
+      }
 
       sendResponse( downloadLink );
     }
