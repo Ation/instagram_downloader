f432059c3ffea7ab65d4ef65c5d6142e29ebcb8b
Ation <gation@gmail.com>
2015-05-22 10:15:54 +0300

    Download images to simplify usage

--- download.js
+++ download.js
@@ -1,19 +1,8 @@
 
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     if (msg.text && (msg.text == "download_media")) {
-      var type = document.querySelectorAll('head meta[property="og:type"]')[0].content;
-      var downloadLink = ''
+      var downloadLink = downloadLink = document.querySelectorAll('head meta[property="og:'+ 'image' +'"]')[0].content;
 
-      switch(type) {
-      case "video":
-        downloadLink = document.querySelectorAll('head meta[property="og:'+ 'video' +'"]')[0].content;
-        break;
-      case "instapp:photo":
-        downloadLink = document.querySelectorAll('head meta[property="og:'+ 'image' +'"]')[0].content;
-        break;
-      default:
-        //
-      }
       sendResponse( downloadLink );
     }
 });
