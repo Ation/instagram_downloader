0bdf12a46b59692658fd2e195a3afe214072e8b4
FilMoskalev <fil.moskalev@gmail.com>
2015-12-28 13:18:33 +0200

    fix download images

--- download.js
+++ download.js
@@ -6,12 +6,19 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
       if (video_tag.length == 1) {
         download_link = video_tag[0].getAttribute("src");
       } else {
-        root = document.body.querySelectorAll('div.-cx-PRIVATE-Photo__root.-cx-PRIVATE-Post__media')
-        if (root.length == 0) {
+        //root = document.body.querySelectorAll('div.-cx-PRIVATE-Photo__root.-cx-PRIVATE-Post__media')
+        root = document.body.querySelectorAll('._ovg3g')
+        /*if (root.length == 0) {
             root = document.body.querySelectorAll('div.-cx-PRIVATE-Photo__root.-cx-PRIVATE-PhotoWithUserTags__photo')
         }
         img = root[0].querySelectorAll('img.-cx-PRIVATE-Photo__image')
-        download_link = img[0].getAttribute('src');
+        download_link = img[0].getAttribute('src');*/
+          var src = root[0].getAttribute('data-reactid').split('https=2//');
+          var download_link_decode = src[1].replace(/=1/g, '.');
+          if (download_link_decode.includes('jpg')){
+              download_link = 'https://' + download_link_decode.split('.jpg')[0] + '.jpg'
+          } else
+            download_link = 'https://' + download_link_decode.split('.gif')[0] + '.gif'
       }
 
       sendResponse(download_link);
